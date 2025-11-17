4c0f1e4a3a140705cf614c4a2c3533ab01ba41a6
Ender <131053595+LookItsEnder@users.noreply.github.com>
2025-11-17 15:05:52 -0600

    Initial File
    This file contains everything, will offload a JSON containing vibration data from a Geophone sensor.
    
    WIP

---
+++ Collect-Websocket.py
@@ -0,0 +1,51 @@
+import asyncio
+import time
+import Adafruit_ADS1x15
+import json
+from websockets.asyncio.server import broadcast, serve
+
+async def noop(websocket):
+    await websocket.wait_closed()
+
+async def show_data(server):
+    adc = Adafruit_ADS1x15.ADS1115(address=0x48, busnum=1)
+    GAIN = 16
+    #f = open('Geophone_Data.txt','w')
+    t0 = time.time()
+    t_end = time.time() + 1
+    #time.time() < t_end
+    json_string = '''{
+    "start": 0,
+    "end": 0,
+    "data": [],
+    "network": "WW",
+    "station": "JUK",
+    "location": "02",
+    "channel": "BHN",
+    "sampleRate": 50,
+    "id": "WW.JUK.02.BHN"
+    }'''
+    jukdata = json.loads(json_string)
+    jukdata['start'] = int(round(t0,3)*1000)
+    jukdata['end'] = int(round(t_end,3)*1000)
+    while(time.time() < t_end):#True):
+        value = adc.read_adc_difference(0, gain=GAIN)
+        #f.write(f"{value}\n")
+        jukdata['data'].append(value)
+        
+        await asyncio.sleep(0.01) 
+
+    #f.close()
+    
+    message = json.dumps(jukdata, indent=2)
+    broadcast(server.connections, message)
+    print(len(jukdata['data']))
+    print(f"Data collection took {time.time() - t0:.2f} seconds")
+
+async def main():
+    async with serve(noop, "0.0.0.0", 5678) as server:
+        while(True):
+            await show_data(server)
+
+if __name__ == "__main__":
+    asyncio.run(main())
